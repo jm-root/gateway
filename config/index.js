7a8933e987b9f20abf497b6a11d13fa9016d5121
jeff <jeff@jamma.cn>
2020-04-29 22:31:23 +0800

    fix config

--- config/index.js
+++ config/index.js
@@ -2,7 +2,8 @@ require('log4js').configure(require('./log4js'))
 process.env.NODE_CONFIG_DIR = require('path').join(__dirname)
 const config = require('config')
 
-const { jaeger, no_server_sso: nosso, no_server_acl: noacl } = config
+const { config: { jaeger } = {} } = config.modules['jm-server-jaeger']
+const { no_server_sso: nosso, no_server_acl: noacl } = config
 if (!jaeger) delete config.modules['jm-server-jaeger']
 if (nosso) delete config.modules['jm-server-sso']
 if (noacl) delete config.modules['jm-server-acl']
